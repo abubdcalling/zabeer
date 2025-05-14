4dec406df74988cf667d9acf820c7c833ab5d7ad
zahidulNahid <zahidulnahid7464@gmail.com>
2025-05-14 20:07:19 +0600

    update

--- resources/views/emails/project_request_received.blade.php
+++ resources/views/emails/project_request_received.blade.php
@@ -4,7 +4,13 @@
 <p><strong>Email:</strong> {{ $projectRequest->email_address }}</p>
 <p><strong>Phone:</strong> {{ $projectRequest->phone_number }}</p>
 <p><strong>Company:</strong> {{ $projectRequest->company_name }}</p>
+<p><strong>Customer Address:</strong> {{ $projectRequest->customer_address }}</p>
+<p><strong>Site Location:</strong> {{ $projectRequest->site_location }}</p>
 <p><strong>Service Type:</strong> {{ $projectRequest->type_of_service }}</p>
 <p><strong>Project Description:</strong> {{ $projectRequest->project_description }}</p>
+<p><strong>Requested Time and Date:</strong> {{ $projectRequest->requested_time_and_date }}</p>
+<p><strong>Start Date:</strong> {{ $projectRequest->start_date }}</p>
+<p><strong>Budget Range:</strong> {{ $projectRequest->budget_range }}</p>
+<p><strong>How Did You Know About Us:</strong> {{ $projectRequest->how_do_you_know_about_us }}</p>
 
 <p>Thank you!</p>
